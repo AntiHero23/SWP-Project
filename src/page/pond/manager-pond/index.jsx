336d6b18a2acc42abcdf1c29c09d6e6d2c30acf0
MinhG555 <minhcnse180019@fpt.edu.vn>
2024-10-06 21:11:27 +0700

    Formatted Pond Card abd Pond Manage page

--- src/page/pond/manager-pond/index.jsx
+++ src/page/pond/manager-pond/index.jsx
@@ -3,6 +3,46 @@ import { useNavigate } from "react-router-dom";
 import api from "../../../config/axios";
 import { PlusCircleOutlined } from "@ant-design/icons";
 import PondCard from "../../../component/pond-card";
+import "./index.scss";
+
+/* Test Data to simulate API response
+const testPonds = [
+  {
+    id: 1,
+    pondName: "Sunshine Pond",
+    pondImage: "../../../LoginBg.png",
+    area: 150,
+    depth: 3,
+    volume: 450,
+    drainCount: 2,
+    skimmerCount: 3,
+    amountFish: 25,
+    pumpingCapacity: 500,
+  },
+  {
+    id: 2,
+    pondName: "Blue Lagoon",
+    area: 200,
+    depth: 2.5,
+    volume: 500,
+    drainCount: 1,
+    skimmerCount: 2,
+    amountFish: 40,
+    pumpingCapacity: 750,
+  },
+  {
+    id: 3,
+    pondName: "Emerald Waters",
+    area: 100,
+    depth: 3.2,
+    volume: 320,
+    drainCount: 3,
+    skimmerCount: 4,
+    amountFish: 15,
+    pumpingCapacity: 300,
+  },
+];
+*/
 
 function ManagerPond() {
   const [ponds, setPonds] = useState([]);
@@ -10,6 +50,11 @@ function ManagerPond() {
   const navigate = useNavigate();
 
   useEffect(() => {
+    // Simulate fetching ponds from an API by setting test data
+    //setPonds(testPonds);
+
+     
+    // Uncomment the following lines to use the real API:
     const fetchPonds = async () => {
       try {
         const response = await api.get("pond");
@@ -20,6 +65,7 @@ function ManagerPond() {
       }
     };
     fetchPonds();
+    
   }, []);
 
   const filteredPonds = ponds.filter((pond) =>
@@ -58,4 +104,5 @@ function ManagerPond() {
     </div>
   );
 }
+
 export default ManagerPond;
