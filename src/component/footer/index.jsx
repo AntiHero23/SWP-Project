1a24b1d163f4960ff3400046795b8ef059be6bfc
MinhG555 <minhcnse180019@fpt.edu.vn>
2024-10-08 23:27:12 +0700

    tweaked footer

--- src/component/footer/index.jsx
+++ src/component/footer/index.jsx
@@ -1,40 +1,27 @@
 import React from 'react';
 import { Row, Col } from 'antd';
+import './index.scss'; // Import the SCSS file
 
 function Footer() {
   return (
-    <footer className="footer" style={{ backgroundColor: '#333' }}>
+    <footer className="footer">
       <Row className="footer-content">
-        <Col className="footer-col" xs={24} sm={8}>
-          <h4 className="footer-title" style={{ color: '#fff' }}>Company</h4>
-          <ul className="footer-list">
-            <li><a href="#" style={{ color: '#fff' }}>About Us</a></li>
-            <li><a href="#" style={{ color: '#fff' }}>Careers</a></li>
-            <li><a href="#" style={{ color: '#fff' }}>Blog</a></li>
-          </ul>
+                <Col className="footer-contact" xs={24}>
+          <p className="footer-address">123 Pond Street, Water City, 12345</p>
+          <p className="footer-email">
+            <a href="mailto:contact@pondcompany.com">contact@pondcompany.com</a>
+          </p>
         </Col>
-        <Col className="footer-col" xs={24} sm={8}>
-          <h4 className="footer-title" style={{ color: '#fff' }}>Support</h4>
-          <ul className="footer-list">
-            <li><a href="#" style={{ color: '#fff' }}>Contact Us</a></li>
-            <li><a href="#" style={{ color: '#fff' }}>Help Center</a></li>
-            <li><a href="#" style={{ color: '#fff' }}>Terms of Service</a></li>
-            <li><a href="#" style={{ color: '#fff' }}>Privacy Policy</a></li>
-          </ul>
-        </Col>
-        <Col className="footer-col" xs={24} sm={8}>
-          <h4 className="footer-title" style={{ color: '#fff' }}>Follow Us</h4>
-          <ul className="footer-list">
-            <li><a href="#" style={{ color: '#fff' }}>Facebook</a></li>
-            <li><a href="#" style={{ color: '#fff' }}>Twitter</a></li>
-            <li><a href="#" style={{ color: '#fff' }}>YouTube</a></li>
-          </ul>
+      </Row>
+
+
+      <Row className="footer-bottom">
+        <Col xs={24}>
+          <p className="footer-copy">© 2024 Pond Company. All rights reserved.</p>
         </Col>
       </Row>
     </footer>
-
-  )
+  );
 }
 
 export default Footer;
-
