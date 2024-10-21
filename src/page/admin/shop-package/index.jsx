2bcfa2d352edecb86e44328436991203f44eeb13
AnPTSE183963 <AnPTSE183963@fpt.edu.vn>
2024-10-21 16:58:10 +0700

    delete package for admin

--- src/page/admin/shop-package/index.jsx
+++ src/page/admin/shop-package/index.jsx
@@ -1,37 +1,38 @@
 import React, { useEffect, useState } from "react";
 import api from "../../../config/axios";
 import { render } from "react-dom";
-import { Button } from "antd";
+import { Button, Modal } from "antd";
 import { useForm } from "antd/es/form/Form";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 function ShopPackage() {
   const { id } = useParams();
   const postId = id;
   const [loading, setLoading] = useState(false);
   const [shopPackage, setShopPackage] = useState([]);
-  const [isOpenModal, setIsOpenModal] = useState(false);
+  const [isUpdateModal, setIsUpdateModal] = useState(false);
+  const [isDeleteModal, setIsDeleteModal] = useState(false);
 
   const [form] = useForm();
-
-  const showModal = () => {
-    setIsOpenModal(true);
-  };
-  const handleCancel = () => {
+  const navigate = useNavigate();
+  // const showModal = () => {
+  //   setIsOpenModal(true);
+  // };
+  const handleCancelUpdate = () => {
     form.resetFields();
-    setIsOpenModal(false);
+    setIsUpdateModal(false);
   };
-  const handleSubmit = async (value) => {
-    try {
-      const response = await api.put(
-        `/admin/package/update/${value}`,
-        form.getFieldsValue()
-      );
-      console.log(response);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  // const handleSubmit = async (value) => {
+  //   try {
+  //     const response = await api.put(
+  //       `/admin/package/update/${value}`,
+  //       form.getFieldsValue()
+  //     );
+  //     console.log(response);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
   const fetchShopPackage = async () => {
     setLoading(true);
     try {
@@ -91,6 +92,74 @@ function ShopPackage() {
             <br />
             <b>Number of Posts: </b>
             {shopPackage.numberOfPosts} posts
+            <p>
+              ______________________________________________________________________________________________________________________________________________________________________________________________________________________
+            </p>
+            <br />
+            <Button
+              type="primary"
+              onClick={() => setIsUpdateModal(true)}
+              style={{
+                marginTop: "10px",
+                width: "100px",
+                textAlign: "center",
+              }}
+            >
+              Update
+            </Button>
+            <Modal
+              title="Update Package"
+              style={{
+                textAlign: "center",
+              }}
+              open={isUpdateModal}
+              onOk={() => {
+                api
+                  .put(`/admin/post/approve/${postId}`)
+                  .then(() => {})
+                  .catch((error) => console.log(error));
+                alert("Post approved successfully! Please wait for a while!");
+                setIsUpdateModal(false);
+              }}
+              onCancel={() => {
+                handleCancelUpdate();
+              }}
+            >
+              aaa
+            </Modal>
+            <Button
+              type="primary"
+              danger
+              onClick={() => setIsDeleteModal(true)}
+              style={{
+                marginTop: "10px",
+                marginLeft: "10px",
+                width: "100px",
+              }}
+            >
+              Delete
+            </Button>
+            <Modal
+              title="Are you sure you want to Delete this package?"
+              style={{
+                textAlign: "center",
+              }}
+              open={isDeleteModal}
+              onOk={() => {
+                api
+                  .delete(`/admin/package/delete/${postId}`)
+                  .then(() => {
+                    fetchShopPackage();
+                  })
+                  .catch((error) => console.log(error));
+                alert("Post rejected successfully!");
+                setTimeout(() => navigate("/admin/package"), 1000);
+                setIsDeleteModal(false);
+              }}
+              onCancel={() => {
+                setIsDeleteModal(false);
+              }}
+            />
             <p>
               ______________________________________________________________________________________________________________________________________________________________________________________________________________________
             </p>
--- src/page/admin/user-package/index.jsx
+++ src/page/admin/user-package/index.jsx
@@ -1,35 +1,38 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import api from "../../../config/axios";
 import { useForm } from "antd/es/form/Form";
+import { Button, Modal } from "antd";
 
 function MemberPackage() {
   const { id } = useParams();
   const postId = id;
   const [loading, setLoading] = useState(false);
   const [memberPackage, setMemberPackage] = useState([]);
-  const [isOpenModal, setIsOpenModal] = useState(false);
+  const [isDeleteModal, setIsDeleteModal] = useState(false);
+  const [isUpdateModal, setIsUpdateModal] = useState(false);
 
   const [form] = useForm();
+  const navigate = useNavigate();
 
-  const showModal = () => {
-    setIsOpenModal(true);
-  };
-  const handleCancel = () => {
+  // const showUpdateModal = () => {
+  //   setIsUpdateModal(true);
+  // };
+  const handleCancelUpdate = () => {
     form.resetFields();
-    setIsOpenModal(false);
+    setIsUpdateModal(false);
   };
-  const handleSubmit = async (value) => {
-    try {
-      const response = await api.put(
-        `/admin/package/update/${value}`,
-        form.getFieldsValue()
-      );
-      console.log(response);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  // const handleSubmit = async (value) => {
+  //   try {
+  //     const response = await api.put(
+  //       `/admin/package/update/${value}`,
+  //       form.getFieldsValue()
+  //     );
+  //     console.log(response);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
   const fetchMemberPackage = async () => {
     setLoading(true);
     try {
@@ -46,19 +49,116 @@ function MemberPackage() {
   useEffect(() => {
     fetchMemberPackage();
   }, []);
+  const VND = new Intl.NumberFormat("vi-VN", {
+    style: "currency",
+    currency: "VND",
+  });
   return (
     <>
-      <div>
+      <div style={{ textAlign: "center" }}>
         <h1>Member Package</h1>
-        <br />
-        <br />
         {!loading && (
           <div>
+            <p>
+              ______________________________________________________________________________________________________________________________________________________________________________________________________________________
+            </p>
+            <br />
             <h2>{memberPackage.name}</h2>
-            <p>Role: {memberPackage.role}</p>
-            <p>Description: {memberPackage.description}</p>
-            <p>Price: {memberPackage.price} VND</p>
-            <p>Duration: {memberPackage.duration} months</p>
+            <p>
+              ______________________________________________________________________________________________________________________________________________________________________________________________________________________
+            </p>
+            <br />
+            <b>Package for {memberPackage.role}</b>
+            <p>
+              ______________________________________________________________________________________________________________________________________________________________________________________________________________________
+            </p>
+            <br />
+            <b>Description: </b>
+            <p>{memberPackage.description}</p>
+            <p>
+              ______________________________________________________________________________________________________________________________________________________________________________________________________________________
+            </p>
+            <br />
+            <b>Price: </b>
+            {VND.format(memberPackage.price)}
+            <p>
+              ______________________________________________________________________________________________________________________________________________________________________________________________________________________
+            </p>
+            <br />
+            <b>Duration: </b>
+            {memberPackage.duration} months
+            <p>
+              ______________________________________________________________________________________________________________________________________________________________________________________________________________________
+            </p>
+            <br />
+            <Button
+              type="primary"
+              onClick={() => setIsUpdateModal(true)}
+              style={{
+                marginTop: "10px",
+                width: "100px",
+                textAlign: "center",
+              }}
+            >
+              Update
+            </Button>
+            <Modal
+              title="Update Package"
+              style={{
+                textAlign: "center",
+              }}
+              open={isUpdateModal}
+              onOk={() => {
+                api
+                  .put(`/admin/post/approve/${postId}`)
+                  .then(() => {})
+                  .catch((error) => console.log(error));
+                alert("Post approved successfully! Please wait for a while!");
+                setIsUpdateModal(false);
+              }}
+              onCancel={() => {
+                handleCancelUpdate();
+              }}
+            >
+              aaa
+            </Modal>
+            <Button
+              type="primary"
+              danger
+              onClick={() => setIsDeleteModal(true)}
+              style={{
+                marginTop: "10px",
+                marginLeft: "10px",
+                width: "100px",
+              }}
+            >
+              Delete
+            </Button>
+            <Modal
+              title="Are you sure you want to Delete this package?"
+              style={{
+                textAlign: "center",
+              }}
+              open={isDeleteModal}
+              onOk={() => {
+                api
+                  .delete(`/admin/package/delete/${postId}`)
+                  .then(() => {
+                    fetchMemberPackage();
+                  })
+                  .catch((error) => console.log(error));
+                alert("Post rejected successfully!");
+                setTimeout(() => navigate("/admin/package"), 1000);
+                setIsDeleteModal(false);
+              }}
+              onCancel={() => {
+                setIsDeleteModal(false);
+              }}
+            />
+            <p>
+              ______________________________________________________________________________________________________________________________________________________________________________________________________________________
+            </p>
+            <br />
           </div>
         )}
       </div>
